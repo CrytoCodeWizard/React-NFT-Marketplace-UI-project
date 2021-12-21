9a0df4090316b47e791c70d4ce5a9e801c4f438e
David Teo <starrynightskyhoney@gmail.com>
2021-12-21 10:06:39 +0100

    ContentBox styling update for non image version

--- src/iotabots/ContentBox/ContentBox.tsx
+++ src/iotabots/ContentBox/ContentBox.tsx
@@ -23,17 +23,17 @@ export const ContentBox: React.FC<ContentBoxProps> = (props) => {
       <Box className={classes.root}>
         <Box className={classes.clipHeader} />
         <Box className={classes.contentWrapper}>
+          {image && (
+            <Box className={classes.imageWrapper}>
+              <Box component='img' src={image} className={classes.image} />
+            </Box>
+          )}
           <Box className={classes.textWrapper}>
             <Box className={classes.textHeadline}>
               <Typography variant='h4'>{headline}</Typography>
             </Box>
             <Typography className={classes.textBody}>{text}</Typography>
           </Box>
-          {image && (
-            <Box className={classes.imageWrapper}>
-              <Box component='img' src={image} className={classes.image} />
-            </Box>
-          )}
         </Box>
         <Box className={classes.clipFooter} />
       </Box>
@@ -61,34 +61,25 @@ const useStyles = makeStyles((theme: Theme) => {
       zIndex: -1,
       // prettier-ignore
       clipPath: 'polygon(0 0, 0 100%, calc(100% - 80px) 100%, 100% calc(100% - 80px), 100% 0)',
-      [theme.breakpoints.down('sm')]: {
+      [theme.breakpoints.down('md')]: {
         // prettier-ignore
         clipPath: 'polygon(0 0, 0 100%, calc(100% - 40px) 100%, 100% calc(100% - 40px), 100% 0)'
       }
     },
     contentWrapper: {
       backgroundColor: theme.palette.background.paper,
-      display: 'flex',
-      [theme.breakpoints.down('sm')]: {
-        flexDirection: 'column'
-      },
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
         justifyContent: 'space-between'
       }
     },
     textWrapper: {
       marginBottom: '-40px',
-      [theme.breakpoints.down('sm')]: {
-        paddingRight: theme.spacing(4)
-      },
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('md')]: {
         flexBasis: 'auto',
         flexGrow: 1,
         flexShrink: 1,
-        paddingRight: '137px'
-      },
-      [theme.breakpoints.up('md')]: {
-        paddingRight: theme.spacing(6)
+        order: 1
       }
     },
     textHeadline: {
@@ -97,6 +88,7 @@ const useStyles = makeStyles((theme: Theme) => {
       color: theme.palette.common.white,
       display: 'inline-flex',
       marginTop: '-20px',
+      maxWidth: 'calc(100% - 20px)',
       paddingBottom: theme.spacing(4),
       paddingLeft: theme.spacing(6),
       paddingRight: theme.spacing(6),
@@ -106,8 +98,7 @@ const useStyles = makeStyles((theme: Theme) => {
         hyphens: 'auto',
         overflow: 'hidden',
         wordBreak: 'break-word',
-        wordWrap: 'break-word',
-        maxWidth: 'calc(100% - 40px)'
+        wordWrap: 'break-word'
       },
       // prettier-ignore
       clipPath: 'polygon(20px 0, 0 20px, 0 100%, calc(100% - 20px) 100%, 100% calc(100% - 20px), 100% 0)'
@@ -118,19 +109,28 @@ const useStyles = makeStyles((theme: Theme) => {
       paddingBottom: 0
     },
     imageWrapper: {
-      position: 'absolute',
-      top: 0,
-      right: 0,
       [theme.breakpoints.down('sm')]: {
-        filter: `drop-shadow(-6px 6px 0px ${theme.palette.background.paper})`
+        filter: `drop-shadow(-6px 6px 0px ${theme.palette.background.paper})`,
+        '& + $textWrapper $textHeadline': {
+          maxWidth: 'calc(100% - 40px)'
+        }
+      },
+      [theme.breakpoints.only('sm')]: {
+        '& + $textWrapper $textHeadline': {
+          maxWidth: 'calc(100% - 115px)'
+        }
+      },
+      [theme.breakpoints.down('md')]: {
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        zIndex: 1
       },
       [theme.breakpoints.up('md')]: {
+        order: 2,
         flexBasis: 0,
         flexGrow: 0,
         flexShrink: 1,
-        position: 'relative',
-        top: 'auto',
-        right: 'auto',
         marginBottom: '-40px'
       }
     },
@@ -141,8 +141,8 @@ const useStyles = makeStyles((theme: Theme) => {
         // prettier-ignore
         clipPath: 'polygon(10px 0, 0 10px, 0 100%, calc(100% - 10px) 100%, 100% calc(100% - 10px), 100% 0)'
       },
-      [theme.breakpoints.up('sm')]: {
-        width: '117px',
+      [theme.breakpoints.only('sm')]: {
+        height: '96px', // same height as a short headline
         // prettier-ignore
         clipPath: 'polygon(20px 0, 0 20px, 0 100%, calc(100% - 20px) 100%, 100% calc(100% - 20px), 100% 0)'
       },
