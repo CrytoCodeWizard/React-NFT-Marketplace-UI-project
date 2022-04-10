7f6c53f499046b1d3ed8b4b5302223162d731219
David Teo <starrynightskyhoney@gmail.com>
2022-04-11 00:24:06 +0200

    add localhost RPC_URL

--- src/web3/hooks/connectors.tsx
+++ src/web3/hooks/connectors.tsx
@@ -6,11 +6,12 @@ const RPC_URLS: { [chainId: number]: string } = {
   97: 'https://data-seed-prebsc-1-s1.binance.org:8545',
   1074: 'https://evm.wasp.sc.iota.org',
   1075: 'https://evm.wasp.sc.iota-defi.com/',
-  1076: 'https://evm.iotabot.strangled.net/'
+  1076: 'https://evm.iotabot.strangled.net/',
+  31337: 'http://127.0.0.1:8545/'
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [97, 1074, 1075, 1076]
+  supportedChainIds: [97, 1074, 1075, 1076, 31337]
 })
 
 export const network = new NetworkConnector({
@@ -18,7 +19,8 @@ export const network = new NetworkConnector({
     97: RPC_URLS[97],
     1074: RPC_URLS[1074],
     1075: RPC_URLS[1075],
-    1076: RPC_URLS[1076]
+    1076: RPC_URLS[1076],
+    31337: RPC_URLS[31337]
   },
   defaultChainId: 1
 })
@@ -28,7 +30,8 @@ export const walletconnect = new WalletConnectConnector({
     1: RPC_URLS[1074],
     2: RPC_URLS[1075],
     3: RPC_URLS[1076],
-    4: RPC_URLS[97]
+    4: RPC_URLS[97],
+    5: RPC_URLS[31337]
   },
   qrcode: true
 })
