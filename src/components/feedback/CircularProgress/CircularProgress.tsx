fefadc8906cf40124847bd91868f8c822d32b447
Enjoy-Coding-Actively <165302443+Enjoy-Coding-Actively@users.noreply.github.com>
2024-03-28 18:21:13 +0100

    Update CircularProgress.tsx

--- src/components/feedback/CircularProgress/CircularProgress.tsx
+++ src/components/feedback/CircularProgress/CircularProgress.tsx
@@ -1,1 +1,23 @@
 
+import React from 'react'
+import {
+  CircularProgress as MuiCircularProgress,
+  CircularProgressProps as MuiCircularProgressProps
+} from '@mui/material'
+import makeStyles from '@mui/styles/makeStyles'
+import { Theme } from '../../../theme/types'
+
+export function CircularProgress(props: MuiCircularProgressProps): JSX.Element {
+  const classes = useStyles()
+  return <MuiCircularProgress className={classes.root} {...props} />
+}
+
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    root: {
+      '&.MuiCircularProgress-colorPrimary': {
+        color: theme.palette.primary.main
+      }
+    }
+  }
+})
